42993266e7113558be966280528093c615978819
Mariam Ahmed <mariam.ahmed@kcl.ac.uk>
2019-11-12 14:58:58 +0000

    Updated main - to be completed on branch

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,6 +2,7 @@ package app;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 /**
  * The class to be executed in order to run the app.
@@ -12,14 +13,24 @@ public class App {
     public static void main(String[] args) {
 
         // Create a primary/root frame. 
-        JFrame frame = new JFrame("Malachello");
-        frame.setMinimumSize(new Dimension(800, 400));
+        JFrame frame = new JFrame("Mello");
+        frame.setMinimumSize(new Dimension(1000, 700));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Board creation button
+        JButton make_button =new JButton("Make Board");
+        make_button.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        frame.add(make_button);//adding button in JFrame
+        make_button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                // new board
+            }
+        });
+
         // Create some content 
-        JLabel myLabel = new JLabel("so empty :(" , SwingConstants.CENTER);
-        myLabel.setFont(new Font("Serif", Font.BOLD, 22));
-        myLabel.setBackground(Color.blue);
+        JLabel myLabel = new JLabel("Welcome to Mello" , SwingConstants.CENTER);
+        myLabel.setFont(new Font("Roboto", Font.BOLD, 22));
+        myLabel.setBackground(Color.gray);
         myLabel.setOpaque(true);
         myLabel.setPreferredSize(new Dimension(100, 80));
 
