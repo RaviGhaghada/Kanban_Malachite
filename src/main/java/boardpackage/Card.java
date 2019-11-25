f7483dbb9950ed2a1d120ee7bcb7c250459c33f9
jainmanvi1504 <55800712+jainmanvi1504@users.noreply.github.com>
2019-11-25 15:13:13 +0000

    Add files via upload

--- src/main/java/boardpackage/Card.java
+++ src/main/java/boardpackage/Card.java
@@ -1,23 +1,26 @@
-package boardpackage;
-
+//Manvi Jain
+//25-11-2019
+import java.util.*;
 public class Card {
 
     private String ID;
     private String Text = "";
+    ArrayList<String> members;
 
-    public Card(String IDIn){
+   public Card(String IDIn){
         ID=IDIn;
+       members = new ArrayList<>();
     }
 
     //Changes the text of a card
     //initially the text is ""
     public void editText(String TextIn){
-        Text = TextIn;
+       Text = TextIn;
     }
 
     //gets the text of a card
     public String getText(){
-        return Text;
+       return Text;
     }
 
     //gets the ID of a card
@@ -27,8 +30,17 @@ public class Card {
 
     //changes the ID of the card
     public void setID(String IDIn){
-        ID=IDIn;
+       ID=IDIn;
     }
 
+    //to assign a card to certain members
+    public void addMember(String name){
+       members.add(name);
+    }
+
+    //returns the arraylist of the members assigned to card
+    public ArrayList<String> getMembers(){
+       return members;
+    }
 
 }
